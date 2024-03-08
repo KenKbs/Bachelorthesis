0e26d30f36bc75baf4ba1e001b2765787fcd549b
KenKbs <kenny.kubsch@gmail.com>
2024-03-08 10:44:27 +0100

    Implementation repeaditly test-train beginning

--- Scripts/traintest/LogisticRegressiontt.py
+++ Scripts/traintest/LogisticRegressiontt.py
@@ -5,3 +5,17 @@ Created on Mon Feb 12 18:31:16 2024
 @author: Kenny
 """
 
+from Scripts.util import (
+    get_data,
+    filter_data,
+    generate_table,
+    train_test_split_data)
+
+#Everything starts here
+
+#%% read in
+
+
+data=get_data()
+
+
