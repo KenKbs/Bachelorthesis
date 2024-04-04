9a197b000aece60355009d7e264ad21ad24fc1f5
KenKbs <kenny.kubsch@gmail.com>
2024-04-04 20:44:42 +0200

    reverted sklearnex changes bec. memory leackage

--- Scripts/gridsearch/SupportVectorMachine.py
+++ Scripts/gridsearch/SupportVectorMachine.py
@@ -179,7 +179,7 @@ param_grid = [rbf_kernel, poly_kernel,linear_kernel]
 
 # TESTING
 # REMOVE LATER AND CHANGE FUNCTION CALL!
-param_grid_t = {'C': [1, 0.01]}
+param_grid_t = {'C': [1, 0.01,2]}
 
 # %%Perform Grid_search
 with parallel_config(temp_folder='/temp',max_nbytes='4M'): #Change temporary folder to where space is (C:/temp) and maxbytes to 4 to avoid memory explosion
